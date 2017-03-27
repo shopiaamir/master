03882deeb5362f5c5bd2c8299e08eaf7703d6f1d
shopiaamir <shopiaamir@gmail.com>
2017-03-28 02:08:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,7 @@
-$('#cssmenu ul ul li:odd').addClass('odd');
-$('#cssmenu ul ul li:even').addClass('even');
-$('#cssmenu > ul > li > a').click(function() {
-  $('#cssmenu li').removeClass('active');
-  $(this).closest('li').addClass('active');
-  var checkElement = $(this).next();
-  if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
-    $(this).closest('li').removeClass('active');
-    checkElement.slideUp('normal');
-  }
-  if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
-    $('#cssmenu ul ul:visible').slideUp('normal');
-    checkElement.slideDown('normal');
-  }
-  if($(this).closest('li').find('ul').children().length == 0) {
-    return true;
-  } else {
-    return false;
-  }
-});
-
-<script type='text/javascript'>
-$(function() {  
+// Menu Dropdown
+$("#cssmenu ul ul li:odd").addClass("odd"),$("#cssmenu ul ul li:even").addClass("even"),$("#cssmenu > ul > li > a").click(function(){$("#cssmenu li").removeClass("active"),$(this).closest("li").addClass("active");var s=$(this).next();return s.is("ul")&&s.is(":visible")&&($(this).closest("li").removeClass("active"),s.slideUp("normal")),s.is("ul")&&!s.is(":visible")&&($("#cssmenu ul ul:visible").slideUp("normal"),s.slideDown("normal")),0==$(this).closest("li").find("ul").children().length});
+// Menu Slide
+$(document).ready( function() {  
     $('.menu').click(function () {
     $('#css-menu').css({right:'0'});
 });
